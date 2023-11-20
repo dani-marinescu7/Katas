58a9a55ff327abc318df351a63bce322666d9327
Daniel <dani.marinescu7@gmail.com>
2023-11-20 23:07:39 +0200

    StringSplit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,14 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import static org.example.StringSplit.solution;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String input = "abcdefg";
+        String[] result = solution(input);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            
-            System.out.println("i = " + i);
+        for (String pair : result) {
+            System.out.println(pair);
         }
     }
 }
---
+++ src/main/java/org/example/StringSplit.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSplit {
+    public static String[] solution(String s) {
+        if (s == null || s.isEmpty()) {
+            return new String[0];
+        }
+
+        int length = s.length();
+        int arrayLength = (length % 2 == 0) ? length / 2 : length / 2 + 1;
+        String[] result = new String[arrayLength];
+
+        for (int i = 0, j = 0; i < length; i += 2, j++) {
+            if (i + 1 < length) {
+                result[j] = s.substring(i, i + 2);
+            } else {
+                result[j] = s.substring(i) + "_";
+            }
+        }
+
+        return result;
+    }
+}
