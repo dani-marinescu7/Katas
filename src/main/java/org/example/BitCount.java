aea3275696065233cad4c330bfad5939a6a828c2
Daniel <dani.marinescu7@gmail.com>
2023-11-22 00:38:31 +0200

    refactored BitCount

--- src/main/java/org/example/BitCount.java
+++ src/main/java/org/example/BitCount.java
@@ -2,25 +2,12 @@ package org.example;
 
 public class BitCount {
 
-    public static int countBits(int n) {
-        int count = 0;
-
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                count++;
-            }
-            n >>= 1;
-        }
-
-        return count;
-    }
+    public static int countBits(int n) { return Integer.bitCount(n); }
 
     public static void main(String[] args) {
-        // Example usage
         int input = 1234;
         int result = countBits(input);
 
-        // Print the result
         System.out.println("Number of set bits: " + result);
     }
 }
