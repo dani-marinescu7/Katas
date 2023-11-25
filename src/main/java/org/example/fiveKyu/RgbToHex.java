7d6d8970f888ecf57252620340f64fa9be1c75be
Daniel <dani.marinescu7@gmail.com>
2023-11-25 03:25:19 +0200

    refactored rgbToHex

--- src/main/java/org/example/fiveKyu/RgbToHex.java
+++ src/main/java/org/example/fiveKyu/RgbToHex.java
@@ -1,13 +1,9 @@
 package org.example.fiveKyu;
 
-import java.math.BigDecimal;
-
 public class RgbToHex {
 
     public static void main(String[] args) {
-
         System.out.println(rgb(-20,275,125));
-
     }
 
     public static String rgb(int r, int g, int b) {
@@ -15,61 +11,29 @@ public class RgbToHex {
         g = converter(g);
         b = converter(b);
 
-        String R = doubleToHexConverter(truncateDecimal((double) r / 16, 2).doubleValue());
-        String G = doubleToHexConverter(truncateDecimal((double) g / 16, 2).doubleValue());
-        String B = doubleToHexConverter(truncateDecimal((double) b / 16, 2).doubleValue());
-
-
-        return R + G + B;
+        return toHex(r) + toHex(g) + toHex(b);
     }
 
-    private static int converter(int i) {
-        if (i < 0) {
-            i = 0;
-        } else if (i > 255) {
-            i = 255;
-        }
-
-        return i;
+    private static int converter(int value) {
+        return Math.min(255, Math.max(0, value));
     }
 
-    private static String doubleToHexConverter(double number) {
-        int x = (int) number;
-        int y = afterDecimalNumber(number);
+    private static String toHex(int number) {
+        int quotient = number / 16;
+        int reminder = number % 16;
 
-        String X = numberToHex(x);
-        String Y = numberToHex(y);
-
-        return X + Y;
+        return hexDigit(quotient) + hexDigit(reminder);
     }
 
-    private static BigDecimal truncateDecimal(double x, int numberofDecimals)
-    {
-        if ( x > 0) {
-            return new BigDecimal(String.valueOf(x)).setScale(numberofDecimals, BigDecimal.ROUND_FLOOR);
-        } else {
-            return new BigDecimal(String.valueOf(x)).setScale(numberofDecimals, BigDecimal.ROUND_CEILING);
-        }
-    }
-
-    private static int afterDecimalNumber(double number) {
-
-        String numberAsString = Double.toString(number);
-        int decimalIndex = numberAsString.indexOf('.');
-        String newNumberAsString = 0 + "." + numberAsString.substring(decimalIndex + 1);
-
-        return (int) Math.ceil(Double.parseDouble(newNumberAsString) * 16);
-    }
-
-    private static String numberToHex(int number) {
-        return switch (number) {
+    private static String hexDigit(int digit) {
+        return switch (digit) {
             case 10 -> "A";
             case 11 -> "B";
             case 12 -> "C";
             case 13 -> "D";
             case 14 -> "E";
             case 15 -> "F";
-            default -> String.valueOf(number);
+            default -> String.valueOf(digit);
         };
     }
 }
