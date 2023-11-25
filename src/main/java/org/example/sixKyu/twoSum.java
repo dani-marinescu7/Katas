a8eb145b438cd47667a6064fe65b66bed21f5a13
Daniel <dani.marinescu7@gmail.com>
2023-11-25 22:49:39 +0200

    twoSum

---
+++ src/main/java/org/example/sixKyu/twoSum.java
@@ -0,0 +1,24 @@
+package org.example.sixKyu;
+
+import java.util.Arrays;
+
+public class twoSum {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3}, 4)));
+    }
+
+    private static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target && i != j) {
+                    result[0] = j;
+                    result[1] = i;
+                }
+            }
+        }
+        return result;
+    }
+}
