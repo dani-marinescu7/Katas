50f6bc4cbf4c5615ca0c755d7db9c436190c99e5
Daniel <dani.marinescu7@gmail.com>
2023-11-29 01:33:05 +0200

    refactored creditcardValidation

--- src/main/java/org/example/sixKyu/creditcardValidation.java
+++ src/main/java/org/example/sixKyu/creditcardValidation.java
@@ -10,31 +10,18 @@ public class creditcardValidation {
         String[] numbers = n.split("");
         int sum = 0;
 
-        if (numbers.length % 2 == 0) {
-            for (int i = 0; i < numbers.length; i++) {
-                int number = Integer.parseInt(numbers[i]);
-                if (i % 2 == 0) {
-                    if ((number * 2) > 9) {
-                        number = (number * 2) - 9;
-                    } else {
-                        number = number * 2;
-                    }
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            int number = Integer.parseInt(numbers[i]);
+            if ((numbers.length - i) % 2 == 0) {
+                number *= 2;
+
+                if (number > 9) {
+                    number = number - 9;
                 }
-                sum += number;
             }
-        } else {
-            for (int i = 0; i < numbers.length; i++) {
-                int number = Integer.parseInt(numbers[i]);
-                if (i % 2 != 0) {
-                    if ((number * 2) > 9) {
-                        number = (number * 2) - 9;
-                    } else {
-                        number = number * 2;
-                    }
-                }
-                sum += number;
-            }
+            sum += number;
         }
+
         return sum % 10 == 0;
     }
 }
