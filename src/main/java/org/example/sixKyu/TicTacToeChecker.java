7891eeeba8cfbca80b7baebcd88887dadbbab9f4
Daniel <dani.marinescu7@gmail.com>
2023-12-02 22:59:27 +0200

    TicTacToeChecker

--- src/main/java/org/example/sixKyu/TicTacToeChecker.java
+++ src/main/java/org/example/sixKyu/TicTacToeChecker.java
@@ -3,11 +3,15 @@ package org.example.sixKyu;
 public class TicTacToeChecker {
 
     public static void main(String[] args) {
-        
+
     }
 
     public static int isSolved(int[][] board) {
-        // your code here
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                
+            }
+        }
         return 0;
     }
 }
