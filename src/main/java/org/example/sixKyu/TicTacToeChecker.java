c3d3968b345375278aab990de103c50bfe316690
Daniel <dani.marinescu7@gmail.com>
2023-12-04 00:43:31 +0200

    solved TicTacToeChecker

--- src/main/java/org/example/sixKyu/TicTacToeChecker.java
+++ src/main/java/org/example/sixKyu/TicTacToeChecker.java
@@ -11,19 +11,28 @@ public class TicTacToeChecker {
     }
 
     public static int isSolved(int[][] board) {
-        for (int[] rows : board) {
-            for (int col = 0; col < rows.length; col++) {
-                if (rows[0] == rows[1] && rows[1] == rows[2] && rows[0] != 0) {
-                    return rows[0];
-                } else if (board[0][col] == board[1][col] && board[1][col] == board[2][col] && board[0][col] != 0) {
-                    return board[0][col];
-                } else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] !=0 || board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] !=0) {
-                    return board[1][1];
-                } else if (rows[col] == 0) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 0) {
+                return board[i][0];
+            }
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != 0) {
+                return board[0][i];
+            }
+        }
+
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != 0) ||
+                (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != 0)) {
+            return board[1][1];
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {
                     return -1;
                 }
             }
         }
         return 0;
     }
+
 }
