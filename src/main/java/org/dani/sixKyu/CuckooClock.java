fa0b1a776cebb2a95dbe6d3044cd32505f01d869
Daniel <dani.marinescu7@gmail.com>
2023-12-11 01:46:23 +0200

    almost working version of CuckooClock

--- src/main/java/org/dani/sixKyu/CuckooClock.java
+++ src/main/java/org/dani/sixKyu/CuckooClock.java
@@ -8,39 +8,29 @@ public class CuckooClock {
 
     public static String cuckooClock(String inputTime, int chimes) {
         String[] splitTime = inputTime.split(":");
-        int currentHour = Integer.parseInt(splitTime[0]);
-        int currentMinute = Integer.parseInt(splitTime[1]);
-        int totalChimes = calculateChimes(currentHour, currentMinute);
-        int remainingChimes = chimes - totalChimes;
+        int hour = Integer.parseInt(splitTime[0]);
+        int minutes = Integer.parseInt(splitTime[1]);
 
-        while (remainingChimes > 0) {
-            currentHour = (currentHour % 12) + 1;
-            totalChimes += calculateChimes(currentHour, 0);
-            remainingChimes = chimes - totalChimes;
+        while (chimes > 0) {
+            if (minutes >= 45) {
+                chimes -= 1;
+                minutes = 0;
+                hour += 1;
+            } else if (minutes >= 30) {
+                chimes -= 1;
+                minutes = 45;
+            } else if (minutes >= 15) {
+                chimes -= 1;
+                minutes = 30;
+            } else if (minutes > 0) {
+                chimes -= 1;
+                minutes = 15;
+            } else if (minutes == 0) {
+                chimes -= 4;
+                minutes = 15;
+            }
         }
 
-        int remainingMinutes = remainingChimes * 15;
-        currentMinute += remainingMinutes;
-
-        return String.format("%02d:%02d", currentHour, currentMinute);
-    }
-
-    private static int calculateChimes(int hour, int minute) {
-        int chimes = 0;
-
-        if (minute >= 45) {
-            chimes += 1;
-        }
-        if (minute % 30 == 0) {
-            chimes += 2;
-        }
-        if (minute % 15 == 0) {
-            chimes += 1;
-        }
-        if (minute == 0) {
-            chimes += hour;
-        }
-
-        return chimes;
+        return String.format("%02d:%02d", hour, minutes);
     }
 }
