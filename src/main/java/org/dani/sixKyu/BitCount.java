722889d013173c268134192857c12234e3d0903c
Daniel <dani.marinescu7@gmail.com>
2023-12-14 23:15:12 +0200

    .

--- src/main/java/org/dani/sixKyu/BitCount.java
+++ src/main/java/org/dani/sixKyu/BitCount.java
@@ -5,7 +5,7 @@ public class BitCount {
     public static int countBits(int n) { return Integer.bitCount(n); }
 
     public static void main(String[] args) {
-        int input = 1234;
+        int input = 123454;
         int result = countBits(input);
 
         System.out.println("Number of set bits: " + result);
