a45795d41f4d06a29f1f928a6590351a0843b826
Daniel <dani.marinescu7@gmail.com>
2024-01-15 23:10:36 +0200

    Started FactDecomp

---
+++ src/main/java/org/dani/fiveKyu/FactDecomp.java
@@ -0,0 +1,25 @@
+package org.dani.fiveKyu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FactDecomp {
+
+    public static void main(String[] args) {
+        decomp(12);
+    }
+
+    public static String decomp(int n) {
+        List<Integer> primeFactors = primeFactors(n);
+        System.out.println(primeFactors);
+        return null;
+    }
+
+    private static List<Integer> primeFactors(int number) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 2; i <= number; i++) {
+
+        }
+        return result;
+    }
+}
