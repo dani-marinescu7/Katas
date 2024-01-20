cc8ecfdd056a38aab655deaa4d2d989bb3a29d8d
Daniel <dani.marinescu7@gmail.com>
2024-01-20 23:24:18 +0200

    starting FromAToZ

---
+++ src/main/java/org/dani/sevenKyu/FromAToZ.java
@@ -0,0 +1,13 @@
+package org.dani.sevenKyu;
+
+public class FromAToZ {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String gimmeTheLetters(String s){
+        //put your lovely code here ^_^
+        return "";
+    }
+}
