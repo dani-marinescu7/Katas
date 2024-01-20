d18c568f3627dc3013dd2f0e436063ea51c8e35f
Daniel <dani.marinescu7@gmail.com>
2024-01-21 00:16:07 +0200

    solved FromAToZ

--- src/main/java/org/dani/sevenKyu/FromAToZ.java
+++ src/main/java/org/dani/sevenKyu/FromAToZ.java
@@ -3,11 +3,19 @@ package org.dani.sevenKyu;
 public class FromAToZ {
 
     public static void main(String[] args) {
-
+        System.out.println(gimmeTheLetters("a-z"));
     }
 
     public static String gimmeTheLetters(String s){
-        //put your lovely code here ^_^
-        return "";
+        char start = s.charAt(0);
+        char end = s.charAt(2);
+
+        StringBuilder result = new StringBuilder();
+
+        for (char current = start; current <= end; current++) {
+            result.append(current);
+        }
+
+        return result.toString();
     }
 }
