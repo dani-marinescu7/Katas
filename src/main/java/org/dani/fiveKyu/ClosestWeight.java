ce5e76feb90c7abf18012171eb4da150aec84672
Daniel <dani.marinescu7@gmail.com>
2024-01-23 23:18:23 +0200

    working on ClosestWeight

--- src/main/java/org/dani/fiveKyu/ClosestWeight.java
+++ src/main/java/org/dani/fiveKyu/ClosestWeight.java
@@ -6,7 +6,7 @@ public class ClosestWeight {
         System.out.println(closest("456899 50 11992 176 272293 163 389128 96 290193 85 52"));
     }
 
-    public static int[][] closest(String strng) {
+    public static int[][] closest(String string) {
         int[][] result = new int[2][3];
 
         return result;
