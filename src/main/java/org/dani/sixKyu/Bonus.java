213d40d675b256438b613e05a35aeab46956f628
Daniel <dani.marinescu7@gmail.com>
2024-01-26 22:48:12 +0200

    Started Bonuses

---
+++ src/main/java/org/dani/sixKyu/Bonus.java
@@ -0,0 +1,15 @@
+package org.dani.sixKyu;
+
+import java.util.Arrays;
+
+public class Bonus {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(bonus(new int[]{18, 15, 12}, 851)));
+    }
+
+    public static long[] bonus(int[] arr, long s) {
+        // Your code
+        return null;
+    }
+}
