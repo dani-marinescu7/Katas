ef45661fbc663338535922ca98b6c3c2ec8b5b47
Daniel <dani.marinescu7@gmail.com>
2024-02-10 18:04:31 +0200

    Refactored Bonus

--- src/main/java/org/dani/sixKyu/Bonus.java
+++ src/main/java/org/dani/sixKyu/Bonus.java
@@ -6,23 +6,10 @@ public class Bonus {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(bonus(new int[]{18, 15, 12}, 851)));
-        //[230, 276, 345]
-        //230 * 18 = 276 * 15 = 345 * 12 and 230 + 276 + 345 = 851
     }
 
     public static long[] bonus(int[] arr, long s) {
-        double[] inverseProportion = new double[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            inverseProportion[i] = 1.0 / arr[i];
-        }
-        double sumOfInverseProportion = Arrays.stream(inverseProportion).sum();
-        long[] calculateBonus = new long[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            calculateBonus[i] = Math.round(inverseProportion[i] / sumOfInverseProportion * s);
-        }
-
-        return calculateBonus;
+        double sumOfInverseProportion = Arrays.stream(arr).mapToDouble(value -> 1.0 / value).sum();
+        return Arrays.stream(arr).mapToLong(value -> Math.round(1.0 / value / sumOfInverseProportion * s)).toArray();
     }
 }
